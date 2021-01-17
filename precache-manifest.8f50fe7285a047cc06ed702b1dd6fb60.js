9df8502df5ae568f6096753e16d911421091fd79
903958734@qq.com <903958734@qq.com>
2021-01-17 21:40:31 +0800

    deploy

---
+++ precache-manifest.8f50fe7285a047cc06ed702b1dd6fb60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "956d36a87e7122e583a1",
+    "url": "/morney-website/css/app.43fa5130.css"
+  },
+  {
+    "revision": "03455fd131bb211048091d03af4b6b77",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "956d36a87e7122e583a1",
+    "url": "/morney-website/js/app.8570884e.js"
+  },
+  {
+    "revision": "f33d9dece1fe2d59bb4a",
+    "url": "/morney-website/js/chunk-vendors.48797907.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
